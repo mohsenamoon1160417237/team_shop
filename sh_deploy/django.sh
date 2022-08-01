1f965c9c42969c0d5c8dba9568a406e32ac6a5e4
mohsenamoon <dramatic225@gmail.com>
2022-08-02 01:04:47 +0430

    start nginx service after collect static

--- sh_deploy/django.sh
+++ sh_deploy/django.sh
@@ -3,4 +3,5 @@
 python manage.py makemigrations &&
   python manage.py migrate &&
   python manage.py collectstatic --noinput &&
+  service nginx start &&
   gunicorn team_shop.wsgi:application --bind 0.0.0.0:8000
