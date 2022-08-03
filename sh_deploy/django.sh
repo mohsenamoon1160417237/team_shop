92e4bd737ee461a2952b9a7ec061065b1ff9459d
mohsenamoon <dramatic225@gmail.com>
2022-08-04 00:18:51 +0430

    group add

--- sh_deploy/django.sh
+++ sh_deploy/django.sh
@@ -2,7 +2,7 @@
 
 pip install -r requirements.txt
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py makemigrations &&
   python manage.py migrate &&
   gunicorn team_shop.wsgi:application --bind 0.0.0.0:8000
